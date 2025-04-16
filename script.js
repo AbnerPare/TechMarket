c7145df5484dab478319dea5e0a33336f6754aef
AbnerPare <abnerpare902@gmail.com>
2025-04-16 10:25:33 +0000

    deuxième commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Exercice 3
+function agrandirImage() {
+    const img = document.getElementById("photo");
+    img.style.width = "300px";
+    img.style.transition = "width 0.1s ease";
+}
+
+function reduireImage() {
+    const img = document.getElementById("photo");
+    img.style.width = "auto";
+}
+
+// photo 1
+function agrandirImage1() {
+    const img = document.getElementById("photo1");
+    img.style.width = "300px";
+    img.style.transition = "width 0.1s ease";
+}
+
+function reduireImage1() {
+    const img = document.getElementById("photo1");
+    img.style.width = "auto";
+}
+
+// photo 2
+function agrandirImage2() {
+    const img = document.getElementById("photo2");
+    img.style.width = "300px";
+    img.style.transition = "width 0.1s ease";
+}
+
+function reduireImage2() {
+    const img = document.getElementById("photo2");
+    img.style.width = "auto";
+}
